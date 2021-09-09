7d212ce44c417db35706735ec02d39c22786a0ea
jf18j492 <jonas.frey@csh.unibe.ch>
2021-09-09 13:33:04 +0200

    added bashfile

---
+++ bgimgchanger/bgimgchanger.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# install cronjob #
+#write out current crontab
+
+
+if grep "bash $scriptpathfilename" /etc/crontab; then
+    echo "crontab already installed"
+else
+
+    if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+    fi
+fi
+
+echo "install crontab if not exists"
+
+scriptpathfilename=$(pwd)/bgimgchanger.sh
+
+grep "bash $scriptpathfilename" /etc/crontab || echo "*  *  *  *  * bash $scriptpathfilename" >> /etc/crontab
+
+
+echo "downloading img from hubbleharvest.ch:8080"
+imgname=$(date +%s).jpg
+downloadedimgsfolder=downloadedimgs
+wget --output-document=./$downloadedimgsfolder/$(date +%s).jpg hubbleharvest.ch:8080
+echo "setting downloaded img as background"
+gsettings set org.gnome.desktop.background picture-options 'scaled'
+gsettings set org.gnome.desktop.background picture-uri "file:///$(pwd)/$downloadedimgsfolder/$imgname" 
